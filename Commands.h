4e0c4e1f08d53c5deb2ac67655c80c76939e7e6c
Ylva <ylva.the.defender@gmail.com>
2023-01-17 23:17:03 +0000

    Actually added the file...

---
+++ Commands.h
@@ -0,0 +1,18 @@
+#ifndef COMMANDS
+#define COMMANDS
+#include "Joystick.h"
+
+static const command step[] = {
+	// Setup controller
+	{ NOTHING,  250 },
+	{ TRIGGERS,   5 },
+	{ NOTHING,  150 },
+	{ TRIGGERS,   5 },
+	{ NOTHING,  150 },
+	{ A,          5 },
+	{ NOTHING,  250 }
+
+
+};
+
+#endif
